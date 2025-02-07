238aa5f988ffde19776ff844e1e74313db9488c5
Edilson Drumond <epdrumond@gmail.com>
2025-02-07 21:14:25 +0000

    Extraction | Upload consolidates cities wheather data

--- src/extract.py
+++ src/extract.py
@@ -12,7 +12,7 @@ load_dotenv()
 VISUALCROSSING_API_KEY = os.getenv("VISUALCROSSING_API_KEY")
 CREDENTIALS_FILE = os.getenv("CREDENTIALS_FILE")
 PATH=os.getenv("PROJECT_PATH")
-RAW_PATH="raw_data/"
+RAW_PATH="raw/"
 
 def fetch_weather_data(city: str, start_date: str, end_date: str) -> pd.DataFrame:
     """
@@ -47,30 +47,43 @@ def main():
     #Load list of cities for wheather data extraction and prepare
     cities = pd.read_csv(PATH + "cities.txt", names=["city_name"])    
     cities["enconded_city_name"] = [parse.quote(city) for city in cities["city_name"]]
-    cities["file_city_name"] = [unidecode(city).lower().replace(" ", "") for city in cities["city_name"]]
 
-    for city_name, enconded_city_name, file_city_name in cities.head(2).values:
-        print(city_name)
+    wheather_df = []
+    stations_df = []
+    for index, row in cities.iterrows():
+        city_name = row["city_name"]
+        enconded_city_name = row["enconded_city_name"]
+        
         wheather_data = fetch_weather_data(
             city=enconded_city_name,
             # start_date=extraction_date,
             # end_date=extraction_date
-            start_date="2025-01-01",
-            end_date="2025-01-02"
+            start_date=extraction_date,
+            end_date=extraction_date
         )
 
-        df, stations_df = format_json_into_dataframe(wheather_data)
+        wheather, stations = format_json_into_dataframe(wheather_data)
+        wheather["city"] = city_name
+        stations["city"] = city_name
 
-        file_name = RAW_PATH + f"wheather_{file_city_name}_{extraction_date_str}.csv"
-        file_path = PATH + file_name
-        df.to_csv(file_path)
+        wheather_df.append(wheather)
+        stations_df.append(stations)
 
-        load_into_gcp_bucket(
-            bucket_name="wheather-data",
-            source_file_path=file_path,
-            destination_blob_name=file_name,
-            credentials_file=CREDENTIALS_FILE
-        )
+    wheather_df = pd.concat(wheather_df)
+    stations_df = pd.concat(stations_df)
+
+    wheather_file_name = RAW_PATH + f"wheather_{extraction_date_str}.csv"
+    stations_file_name = RAW_PATH + f"stations_{extraction_date_str}.csv"
+    
+    wheather_df.to_csv(PATH + wheather_file_name)
+    stations_df.to_csv(PATH + stations_file_name)
+
+    load_into_gcp_bucket(
+        bucket_name="wheather-data",
+        source_file_path=PATH + wheather_file_name,
+        destination_blob_name=wheather_file_name,
+        credentials_file=CREDENTIALS_FILE
+    )
 
 if __name__ == "__main__":
     main()
