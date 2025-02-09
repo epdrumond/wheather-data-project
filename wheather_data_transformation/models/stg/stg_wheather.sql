3fddfe05397018e3339995cc1996f4d45e2291f6
Edilson Drumond <epdrumond@gmail.com>
2025-02-09 11:14:11 +0000

    Transformation | Create stg_wheather and adjust materialization type

---
+++ wheather_data_transformation/models/stg/stg_wheather.sql
@@ -0,0 +1,64 @@
+with src_wheather as (
+    select *
+    from {{ source("wheather_data", "wheather") }}
+)
+
+select 
+    datetime(datetime) as measurement_date,
+    tempmax as max_temperature,
+    tempmin as min_temperature,
+    `temp` as mean_temperature,
+    feelslikemax as max_apparent_temperarature,
+    feelslikemin as min_apparent_temperarature,
+    feelslike as mean_apparent_temperature,
+    humidity as relative_humidity,
+    precip as precipitation,
+    precipprob as precipitation_probability,
+    precipcover as precipitation_coverage,
+    split(
+        regexp_replace(
+            replace(
+                replace(
+                    preciptype, 
+                    '[', ''
+                ), 
+                ']', ''
+            ),
+            r'([\'\"])', ''
+        ),
+        ','
+    ) as precipitation_types,
+    snow,
+    snowdepth as snow_depth,
+    windgust as wind_gust,
+    windspeed as wind_speed,
+    winddir as wind_direction,
+    pressure as sea_level_pressue,
+    cloudcover as cloud_cover,
+    visibility,
+    solarradiation as solar_radiation,
+    solarenergy as solar_energy,
+    uvindex as uv_index,
+    sunrise as sunrise_time,
+    sunset as sunset_time,
+    moonphase,
+    split(conditions, ',') as conditions,
+    split(
+        regexp_replace(
+            replace(
+                replace(
+                    stations, 
+                    '[', ''
+                ), 
+                ']', ''
+            ),
+            r'([\'\"])', ''
+        ),
+        ','
+    ) as stations,
+    latitude,
+    longitude,
+    address as full_city_name,
+    split(address)[0] as city_name,
+    cast(tzoffset as int64) as timezone_offset
+from src_wheather
