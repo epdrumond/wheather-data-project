55b44da3b6f6994fc692ca1ad594ddc85e7e4ebd
Edilson Drumond <epdrumond@gmail.com>
2025-02-11 20:56:47 +0000

    Transformation | Improve incrementality strategy to allow for table backill and upserting records

--- wheather_data_transformation/models/prd/fact_wheather_measurement.sql
+++ wheather_data_transformation/models/prd/fact_wheather_measurement.sql
@@ -1,3 +1,10 @@
+{{
+    config(
+        materialized = 'incremental',
+        unique_key = 'fact_wheather_measurement_id',
+        on_schema_change = 'fail'
+    )
+}}
 with stg_wheather as (
     select *
     from {{ ref("stg_wheather") }}
@@ -44,3 +51,12 @@ select
     stations,
     city_name
 from stg_wheather
+where true 
+{% if is_incremental() %}
+    {% if var("start_date", false) and var("end_date", false) %}
+        and measurement_date >= '{{ var("start_date") }}'
+        and measurement_date <= '{{ var("end_date") }}'
+    {% else %}
+        and measurement_date > (select max(measurement_date from {{ this }}))
+    {% endif %}
+{% endif %}
