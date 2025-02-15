e3ed26d88beae14a14e882cf4a205062e878201a
Edilson Drumond <epdrumond@gmail.com>
2025-02-15 16:47:30 +0000

    Automation | Adjust parameters for update to be based on runtime date

--- dags/wheather_data_workflow.py
+++ dags/wheather_data_workflow.py
@@ -3,20 +3,20 @@ from airflow.operators.python import PythonOperator
 from airflow.operators.bash_operator import BashOperator
 from airflow.models import Variable
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from includes.wheather_utils import extract_wheather_data, load_wheather_data
 
 DBT_PROJECT_DIR = Variable.get("DBT_PROJECT_DIR")
 DBT_PROFILES_DIR = Variable.get("DBT_PROFILES_DIR")
 
+reference_date = datetime.today() - timedelta(days=1)
+start_date_str = (reference_date - timedelta(days=2)).strftime("%Y-%m-%d")
+end_date_str = reference_date.strftime("%Y-%m-%d")
+
 default_args = {
     "owner": "airflow",
     "start_date": datetime(2024, 1, 1)
-}
-default_extraction_args = {
-    "start_date": "2024-01-03",
-    "end_date": "2024-01-04"
 }
 
 dag = DAG(
@@ -26,12 +26,12 @@ dag = DAG(
     catchup=False
 )
 
-extract_data = PythonOperator(
+extract_data = PythonOperator( 
     task_id="extract_data",
     python_callable=extract_wheather_data,
     op_kwargs={
-        "start_date": default_extraction_args["start_date"],
-        "end_date": default_extraction_args["end_date"]
+        "start_date": start_date_str,
+        "end_date": end_date_str
     },
     dag=dag
 )
